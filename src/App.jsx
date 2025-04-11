7ff9c8da992bcc5e365188b483f74fb47fef61f5
Senith Udara <senithudara0000@gmail.com>
2025-04-11 19:17:08 +0530

    initial commit after setting up login function and tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import LoginForm from "./components/auth/LoginForm";
+
+function App() {
+  return (
+    <div>
+      <h1 class="text-4xl text-cyan-700 font-extrabold italic text-center mt-10">
+        Food Delivery App
+      </h1>
+      <LoginForm />
+    </div>
+  );
+}
+export default App;
